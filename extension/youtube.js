4fb512da39954de809511cfbfa021cc65d24706c
DomHynes <dominichynes@gmail.com>
2018-03-22 01:50:49 +1100

    youtube videos have primitive title, yt auth actually responds

--- extension/youtube.js
+++ extension/youtube.js
@@ -32,6 +32,8 @@ module.exports = (nodecg, backendEvents) => {
 								return err;
 							}
 
+							lien.end('success', 200);
+
 							tokenStore.value = {
 								yt: tokens
 							};
@@ -45,62 +47,92 @@ module.exports = (nodecg, backendEvents) => {
 		oauth.setCredentials(newData.yt);
 	});
 
-	nodecg.listenFor('yt:uploadSet', (data, cb) => {
+	nodecg.listenFor('yt:uploadSet', (set, cb) => {
 		let video, progress;
 
 		try {
-			video = fs.createReadStream(path.join(data.video.basePath, data.video.videoName));
+			video = fs.createReadStream(path.join(set.video.basePath, set.video.videoName));
 		} catch (e) {
 			cb(e);
 			return;
 		}
 
-		const upload = yt.videos.insert({
-			resource: {
-				snippet: {
-					title: 'test',
-					description: 'also test'
+		const uploadVideo = (set, tournament, cb) => {
+			const players = set.players.map(teams =>
+				teams.map((player, index, players) => {
+					let name;
+					if (player.sponsored &&
+							player.sponsored.sponsored &&
+							player.sponsored.sponsorName) {
+						name = `${player.sponsored.sponsorName} | ${player.name}`;
+					} else {
+						name = player.name;
+					}
+
+					if (players.length > 1 && index !== players.length - 1) {
+						return `${name},`;
+					}
+
+					return name;
+				}).join(' ')).join(' vs ');
+
+			console.log(players);
+
+			const tournamentName = tournament.data.entities.hashtag ||
+				tournament.data.entities.name;
+
+			const upload = yt.videos.insert({
+				resource: {
+					snippet: {
+						title: `${tournamentName} | ${players}`,
+						description: 'also test'
+					},
+					status: {
+						privacyStatus: 'private',
+						notifySubscribers: false
+					}
 				},
-				status: {
-					privacyStatus: 'private',
-					notifySubscribers: false
+				part: 'snippet, status',
+				media: {
+					body: video
 				}
-			},
-			part: 'snippet, status',
-			media: {
-				body: video
-			}
-		}, (err, resource) => {
-			if (err) {
-				cb(err);
-			}
-			clearInterval(progress);
-
-			console.log(upload);
-			console.log(resource);
-
-			data.video.snippet = resource.snippet;
-			data.video.id = resource.id;
-
-			backendEvents.emit('db:setDoc', data, (err, resp) => {
+			}, (err, resource) => {
 				if (err) {
-					console.log(err);
+					cb(err);
 				}
-				data._rev = resp.rev;
-				data.video.uploaded = upload.req.connection._bytesDispatched;
+				clearInterval(progress);
+
+				console.log(upload);
+				console.log(resource);
+
+				set.video.snippet = resource.snippet;
+				set.video.id = resource.id;
+
+				backendEvents.emit('db:setDoc', set, (err, resp) => {
+					if (err) {
+						console.log(err);
+					}
+					set._rev = resp.rev;
+					set.video.uploaded = upload.req.connection._bytesDispatched;
+				});
+
+				cb(null, resource);
 			});
 
-			cb(null, data);
-		});
+			progress = setInterval(() => {
+				backendEvents.emit('db:setDoc', set, (err, resp) => {
+					if (err) {
+						console.log(err);
+					}
+					set._rev = resp.rev;
+					set.video.uploaded = upload.req.connection._bytesDispatched;
+				});
+			}, 1000);
+		};
 
-		progress = setInterval(() => {
-			backendEvents.emit('db:setDoc', data, (err, resp) => {
-				if (err) {
-					console.log(err);
-				}
-				data._rev = resp.rev;
-				data.video.uploaded = upload.req.connection._bytesDispatched;
-			});
-		}, 1000);
+		const tournaments = nodecg.readReplicant('tournamentDB');
+		console.log(tournaments);
+		const tournament = tournaments.find(tournament => tournament.data.entities.tournament.shortSlug === set.tournament.slug);
+		uploadVideo(set, tournament, cb);
 	});
 };

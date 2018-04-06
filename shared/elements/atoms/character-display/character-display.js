caa336f8fb36a85beb4ce1a790aba13cd920a681
DomHynes <dominichynes@gmail.com>
2018-04-07 01:41:27 +1000

    character display element

---
+++ shared/elements/atoms/character-display/character-display.js
@@ -0,0 +1,34 @@
+class CharacterDisplay extends Polymer.Element {
+	static get is() {
+		return 'character-display';
+	}
+
+	static get properties() {
+		return {
+			colourIndex: {
+				type: Number
+			},
+			characterIndex: {
+				type: Number
+			},
+			selectedGame: {
+				type: Number
+			}
+
+		};
+	}
+
+	_findColourOffset(x, y, selectedGame) {
+		if (x === undefined || y === undefined || selectedGame === undefined) {
+			return;
+		}
+		this.updateStyles({
+			'--character-image': `url('/bundles/mm-dashboard/shared/games/${selectedGame.name}/dashboard.png')`
+		});
+		if (selectedGame !== undefined) {
+			return `background-position: ${x * -(selectedGame.images.dashboard.scaleX)}px ${y * -(selectedGame.images.dashboard.scaleY)}px;`;
+		}
+	}
+}
+
+customElements.define(CharacterDisplay.is, CharacterDisplay);

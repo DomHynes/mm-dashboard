1e79209804a6669ec23791c010a2d3ea8c7b8622
DomHynes <dominichynes@gmail.com>
2018-05-06 13:42:22 +1000

    twitch clips basically work

--- dashboard/elements/atoms/socials-twitch/socials-twitch.js
+++ dashboard/elements/atoms/socials-twitch/socials-twitch.js
@@ -1,20 +1,41 @@
 (function () {
+	const clipData = nodecg.Replicant('clipData');
+
 	class socialsPanel extends Polymer.Element {
 		static get is() {
 			return 'socials-twitch';
 		}
+
 		static get properties() {
-		  return {
-		    channel: {
-		      type: String
-        }
-      }
-    }
+			return {
+				channel: {
+					type: String
+				},
+				editURL: {
+					type: String
+				},
+				clipID: {
+					type: String
+				},
+				clipDate: {
+					type: String
+				}
+			};
+		}
 
 		getClip() {
 			nodecg.sendMessage('twitch:getClip', this.channel, (err, data) => {
-			  console.log(err, data);
-      });
+				console.log(err, data);
+			});
+		}
+
+		ready() {
+			super.ready();
+			clipData.on('change', newData => {
+				this.editURL = newData.edit_url;
+				this.clipID = newData.id;
+				this.clipDate = newData.timestamp;
+			});
 		}
 	}
 
--- extension/twitch/twitch.js
+++ extension/twitch/twitch.js
@@ -5,30 +5,51 @@ const CircularJSON = require('circular-json');
 
 module.exports = (nodecg, backendEvents) => {
 	const tokenStore = nodecg.Replicant('token-store');
+	const clipData = nodecg.Replicant('clipData', {
+		defaultValue: {
+			timestamp: '',
+			edit_url: '',
+			id: ''
+		}
+	});
 
 	const helix = axios.create({
 		baseURL: 'https://api.twitch.tv/helix/'
 	});
+
+	helix.interceptors.response.use(
+		response => response,
+		err => {
+			if (err.response.status === 401 && err.response) {
+				backendEvents.call('twitch:refreshToken');
+			}
+			return err;
+		}
+	);
 
 	tokenStore.on('change', async newData => {
 		helix.defaults.headers.common.Authorization = `Bearer ${newData.twitch.accessToken}`;
 	});
 
 	tokenStore.once('change', async newData => {
-    nodecg.listenFor('twitch:getClip', async (channel, cb) => {
-    	console.log(channel);
-    	try {
-        const broadcasterID = await helix.get('/users', {
-          params: {login: channel},
-        });
-        const clip = await helix.post('/clips', {
-          broadcaster_id: broadcasterID.data.data[0].id
-        });
-        cb(null, clip.data);
-      } catch (e) {
-    		console.log(e);
-    		cb({message: e.message});
+		nodecg.listenFor('twitch:getClip', async (channel, cb) => {
+			console.log(channel);
+			try {
+				const broadcasterID = await helix.get('/users', {
+					params: {login: channel}
+				});
+				const clip = await helix.post('/clips', {
+					broadcaster_id: broadcasterID.data.data[0].id
+				});
+				clipData.value = Object.assign({}, clip.data.data[0], {
+					timestamp: Date.now()
+				});
+				console.log(clipData.value);
+				cb(null, clip.data);
+			} catch (e) {
+				console.log(e);
+				cb({message: e.message});
 			}
-    });
-	})
+		});
+	});
 };
